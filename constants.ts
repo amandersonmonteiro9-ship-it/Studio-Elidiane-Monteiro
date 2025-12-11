c8ac54c462edc921e00a32e2f31d85e2264f7597
amandersonmonteiro9-ship-it <amandersonmonteiro9@gmail.com>
2025-12-11 13:12:24 -0300

    preço de mecha

--- constants.ts
+++ constants.ts
@@ -9,7 +9,7 @@ export const SERVICES: Service[] = [
   { id: '4', name: 'Hidratação Profunda', price: 120, category: 'cabelo', description: 'Devolva a vida e maciez aos seus fios.' },
   { id: '5', name: 'Banho de Brilho', price: 100, category: 'cabelo', description: 'Revitalize a cor e o brilho do seu cabelo.' },
   { id: '6', name: 'Coloração', price: 150, category: 'cabelo', description: 'Cobertura perfeita e cores vibrantes.' },
-  { id: '7', name: 'Teste de Mechas', price: 50, category: 'cabelo', description: 'Segurança e saúde antes da química.' },
+  { id: '7', name: 'Teste de Mechas', price: 40, category: 'cabelo', description: 'Segurança e saúde antes da química.' },
   { id: '8', name: 'Designer de Sobrancelhas', price: 40, category: 'sobrancelha' },
   { id: '9', name: 'Designer com Henna', price: 55, category: 'sobrancelha' },
   { id: '10', name: 'Micropigmentação', price: 450, category: 'sobrancelha', description: 'Sobrancelhas perfeitas e duradouras.' },
